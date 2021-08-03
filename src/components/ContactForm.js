77dfe3c5cf72e7962adf5296a2c58c39b2927f7a
Sebastian Tramper <tramper.s@outlook.com>
2021-08-03 21:40:13 +0200

    made the contact form work

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,7 +6,6 @@ function ContactForm() {
     return (
         <Form name="contact" method="post">
             <input type="hidden" name="form-name" value="contact" />
-            <input type="hidden" name="form-name" value="contact" />
             <input type="text" className="form-control rounded-pill mb-3" name="name" placeholder="Naam" />
             <input type="text" className="form-control rounded-pill mb-3" name="email" placeholder="Email" />
             <textarea className="form-control rounded-3 mb-3" name="Message" type="text" placeholder="Bericht" />
